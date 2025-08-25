ee3f910a9ac936508c28a53bab6c301da421ec88
SansyHuman <suboo0308@gmail.com>
2025-08-25 14:38:23 +0900

    Test2

--- test/text_clustering_test.py
+++ test/text_clustering_test.py
@@ -1,6 +1,7 @@
 import csv
 from time import time
 import numpy as np
+import matplotlib.pyplot as plt
 
 from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -33,6 +34,8 @@ for i in range(1, len(data)):
     w_text = w_text[:-1]
     superpotentials.append(w_text)
 
+rational = [data[i + 1][rational_index] != '' for i in range(len(superpotentials))]
+
 vectorizer = TfidfVectorizer()
 
 t0 = time()
@@ -41,7 +44,25 @@ superpotentials_vector = vectorizer.fit_transform(superpotentials)
 print(f'Vectorization took {time() - t0} seconds.')
 print(f"samples: {superpotentials_vector.shape[0]}, features: {superpotentials_vector.shape[1]}")
 
-kmeans = KMeans(n_clusters=16)
+kmeans = KMeans(n_clusters=32)
 kmeans.fit(superpotentials_vector)
 cluster_ids, cluster_sizes = np.unique(kmeans.labels_, return_counts=True)
 print(f"Number of elements assigned to each cluster: {cluster_sizes}")
+
+num_rationals = [0] * len(cluster_ids)
+for i in range(len(rational)):
+    if rational[i]:
+        num_rationals[kmeans.labels_[i]] += 1
+
+x = np.arange(len(num_rationals))
+ratio = np.array(num_rationals) / np.array(cluster_sizes)
+
+plt.rcParams["figure.figsize"] = (16, 12)
+plt.bar(x, ratio)
+plt.xticks(x, cluster_ids)
+
+plt.title('Ratio of rational central charges in cluster')
+plt.xlabel('Cluster')
+plt.ylabel('Ratio of rational central charges')
+
+plt.show()
