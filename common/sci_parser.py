a1817da6872a2aece33703aa1b665e856c0bbd3e
SansyHuman <suboo0308@gmail.com>
2025-09-17 12:59:11 +0900

    Added sci parser.

---
+++ common/sci_parser.py
@@ -0,0 +1,42 @@
+from common.utils import *
+
+marginal = Monomial('t', 6)
+spectral = Monomial('y', 0)
+
+
+class SuperConformalIndex:
+    """
+    Class which contains the information of a superconformal index.
+    """
+    def __init__(self, index: str) -> None:
+        """
+        Converts the index string into a polynomial object and parses information.
+        :param index: index string.
+        """
+        self.index = to_poly(index)
+        # terms with exponent of t less than 6
+        self.short_index = Polynomial(
+            *filter(lambda term: term.exponent('t') < 6,
+                    filter(lambda term2: term2.exponent('t') is not None, self.index.terms))
+        )
+        # # of marginal operators - rank of IR flavor symmetry
+        self.num_dim3_minus_f = self.index.find_with(marginal)[0].coefficient
+
+        # dimensions of relevant operators
+        self.relevant_dims: set[float] = set()
+        # the number of operators with each dimensions
+        self.relevant_spectrum: dict[float, int] = dict()
+        # total number of relevant operators
+        self.num_relevant_ops = 0
+        terms_spectrum = self.short_index.find_with(spectral)
+        for term in terms_spectrum:
+            dim = term.exponent('t')
+            if dim is not None:
+                dim = dim / 2.0 # t^3R, dim = 3R/2
+                cnt = round(term.coefficient)
+                self.relevant_dims.add(dim)
+                if dim in self.relevant_spectrum:
+                    self.relevant_spectrum[dim] += cnt
+                else:
+                    self.relevant_spectrum[dim] = cnt
+                self.num_relevant_ops += cnt
