4cf6415ff64e72f96c2c3834a3decc78a8e1577b
SansyHuman <suboo0308@gmail.com>
2025-09-28 23:11:20 +0900

    Revised SCI parser to get dimensions of entire operators.

--- common/sci_parser.py
+++ common/sci_parser.py
@@ -25,24 +25,35 @@ class SuperConformalIndex:
         if len(marginal_term) > 0:
             self.num_dim3_minus_f = round(self.index.find_with(marginal)[0].coefficient)
 
+        # dimensions of operators in the order of increasing
+        self.dims: list[float] = []
         # dimensions of relevant operators in the order of increasing
         self.relevant_dims: list[float] = []
         # the number of operators with each dimensions
         self.relevant_spectrum: dict[float, int] = dict()
         # total number of relevant operators
         self.num_relevant_ops = 0
-        terms_spectrum = self.short_index.find_with(spectral)
+
+        terms_spectrum = self.index.find_with(spectral)
+        tmp_dims = set()
         for term in terms_spectrum:
             dim = term.exponent('t')
             if dim is not None:
                 dim = dim / 2.0 # t^3R, dim = 3R/2
                 cnt = round(term.coefficient)
-                if dim in self.relevant_spectrum:
-                    self.relevant_spectrum[dim] += cnt
-                else:
-                    self.relevant_dims.append(dim)
-                    self.relevant_spectrum[dim] = cnt
-                self.num_relevant_ops += cnt
+
+                if dim < 3.0: # relevant
+                    if dim in self.relevant_spectrum:
+                        self.relevant_spectrum[dim] += cnt
+                    else:
+                        self.relevant_dims.append(dim)
+                        self.relevant_spectrum[dim] = cnt
+                    self.num_relevant_ops += cnt
+
+                tmp_dims.add(dim)
+
+        self.dims = list(tmp_dims)
+        self.dims.sort()
         self.relevant_dims.sort()
         # smallest dimension among all operators
         self.smallest_dim = self.relevant_dims[0]
