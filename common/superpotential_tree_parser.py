83387fb4a8099332fb49b9eb02d94318a926c7d3
SansyHuman <suboo0308@gmail.com>
2026-02-12 23:32:11 +0900

    Added program to plot 3 - DeltaW and delta ac.

---
+++ common/superpotential_tree_parser.py
@@ -0,0 +1,132 @@
+import csv
+import ast
+
+import numpy as np
+
+
+class TheoryData:
+    def __init__(self, id, w, a, c, r):
+        self.id = id
+        self.w = w
+        self.a = a
+        self.c = c
+        self.r = r
+
+
+class SuperpotentialTreeNode:
+    def __init__(self, theory_data: TheoryData, added_term=''):
+        self.theory_data: TheoryData = theory_data
+        self.added_term: str = added_term
+        self.children: list[SuperpotentialTreeNode] = []
+
+
+def refine_r_charge_string(raw_r: str) -> str:
+    r = '{' + raw_r[1:-1] + '}'
+
+    i = 0
+    while True:
+        if r[i] == ':':
+            for j in range(i - 1, -1, -1):
+                if r[j] == ' ' or r[j] == '{':
+                    r = r[:j + 1] + '\"' + r[j + 1:i] + '\"' + r[i:]
+                    i += 2
+                    break
+
+        i += 1
+        if i >= len(r):
+            break
+
+    return r
+
+
+def build_superpotential_tree(filename: str) -> tuple[str, SuperpotentialTreeNode]:
+    """
+    Builds a superpotential tree from the given file.
+    :param filename: csv file that contains landscape data of only one theory.
+    :return: Superpotential tree where children is the superpotential which adds one
+    relevant gauge-invariant term from parent.
+    """
+    csv.field_size_limit(np.iinfo(np.int32).max)
+
+    data = None
+    with open(filename) as csvfile:
+        reader = csv.reader(csvfile)
+        data = list(reader)
+
+    id_index, theory_index, w_index, a_index, c_index, r_index = -1, -1, -1, -1, -1, -1
+    for i in range(len(data[0])):
+        if data[0][i] == "id":
+            id_index = i
+        elif data[0][i] == "Name":
+            theory_index = i
+        elif data[0][i] == "Superpotentials":
+            w_index = i
+        elif data[0][i] == "CentralChargeA":
+            a_index = i
+        elif data[0][i] == "CentralChargeC":
+            c_index = i
+        elif data[0][i] == "Rcharges":
+            r_index = i
+
+    theory_name = data[1][theory_index]
+
+    theory_data = []
+
+    for i in range(1, len(data)):
+        id = int(data[i][id_index])
+        w = data[i][w_index]
+        w = w[1:-1].split(',')
+        w_terms = set()
+        for j in range(len(w)):
+            w_term = w[j].strip()
+            if w_term != '':
+                w_terms.add(w_term)
+
+        a = float(data[i][a_index])
+        c = float(data[i][c_index])
+
+        r = refine_r_charge_string(data[i][r_index])
+        r_charges = ast.literal_eval(r)
+
+        theory_data.append(TheoryData(id, w_terms, a, c, r_charges))
+
+    theory_by_length = dict()
+    max_length = -1
+    for theory in theory_data:
+        w_length = len(theory.w)
+        if w_length > max_length:
+            max_length = w_length
+        if w_length not in theory_by_length:
+            theory_by_length[w_length] = []
+        theory_by_length[w_length].append(theory)
+
+    superpotential_tree = SuperpotentialTreeNode(theory_by_length[0][0])
+
+    for theory in theory_by_length[1]:
+        term = theory.w.copy()
+        superpotential_tree.children.append(SuperpotentialTreeNode(theory, term.pop()))
+
+    for length in range(2, max_length + 1):
+        for theory in theory_by_length[length]:
+            parent_candidate = superpotential_tree
+            remaining_terms = theory.w.copy()
+
+            found_candidate = False
+            for _ in range(length - 1):
+                found_candidate = False
+
+                for candidate in parent_candidate.children:
+                    if candidate.added_term in remaining_terms:
+                        parent_candidate = candidate
+                        remaining_terms.remove(candidate.added_term)
+                        found_candidate = True
+                        break
+
+                if not found_candidate:
+                    break
+
+            if not found_candidate:
+                continue
+            parent_candidate.children.append(SuperpotentialTreeNode(theory, remaining_terms.pop()))
+
+    return theory_name, superpotential_tree
---
+++ main_superpotential_charge_diff_calc.py
@@ -0,0 +1,96 @@
+import csv
+import sys
+import os.path
+import math
+import json
+import pathlib
+
+from common.superpotential_parser import serialize_w_term_list
+from common.superpotential_tree_parser import build_superpotential_tree, SuperpotentialTreeNode
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+filename = input('Enter file name that contains data of landscape from one gauge theory: ')
+theory_name, superpotential_tree = build_superpotential_tree(filename)
+
+
+def build_dataset(tree: SuperpotentialTreeNode, dw_dim_data: list[float], da_data: list[float], dc_data: list[float]):
+    r_charges = tree.theory_data.r
+    for child in tree.children:
+        dw = serialize_w_term_list([child.added_term])[0]
+        dw_dim = 0
+
+        add_data = True
+        for op, index, exp in dw:
+            if op not in r_charges or index - 1 >= len(r_charges[op]):
+                add_data = False
+                break
+            else:
+                dw_dim += r_charges[op][index - 1] * exp
+
+        if dw_dim >= 2:
+            add_data = False
+
+        da = child.theory_data.a - tree.theory_data.a
+        dc = child.theory_data.c - tree.theory_data.c
+        if da >= 0 or dc >= 0:
+            add_data = False
+
+        if add_data:
+            dw_dim *= 1.5
+
+            dw_dim_data.append(dw_dim)
+            da_data.append(da)
+            dc_data.append(dc)
+
+        build_dataset(child, dw_dim_data, da_data, dc_data)
+
+
+dw_dim_data = []
+da_data = []
+dc_data = []
+
+build_dataset(superpotential_tree, dw_dim_data, da_data, dc_data)
+
+print(f"Data size: {len(dw_dim_data)}")
+
+three_minus_dim = np.full_like(len(dw_dim_data), 3.0) - np.array(dw_dim_data)
+da = -np.array(da_data)
+dc = -np.array(dc_data)
+
+log_dim = np.log(three_minus_dim)
+log_da = np.log(da)
+log_dc = np.log(dc)
+
+za = np.polyfit(log_dim, log_da, 1)
+zc = np.polyfit(log_dim, log_dc, 1)
+pa = np.poly1d(za)
+pc = np.poly1d(zc)
+
+min_dim = np.min(three_minus_dim)
+max_dim = np.max(three_minus_dim)
+dim_plot = np.linspace(min_dim, max_dim, 100)
+
+plt.style.use('default')
+plt.rcParams['figure.figsize'] = (16, 12)
+plt.rcParams['font.size'] = 15
+
+fig, ax = plt.subplots(1, 2, sharey=True, squeeze=True)
+fig.suptitle(f'Plot of 3-Delta and delta a and delta c of {theory_name} theory')
+
+ax[0].scatter(three_minus_dim, da_data, s=1)
+ax[0].plot(dim_plot, -np.exp(pa(np.log(dim_plot))), 'r--')
+ax[0].text(0.1, 0, f'delta a = {-np.exp(za[1]):.3f}*epsilon^{za[0]:.3f}')
+ax[1].scatter(three_minus_dim, dc_data, s=1)
+ax[1].plot(dim_plot, -np.exp(pc(np.log(dim_plot))), 'r--')
+ax[1].text(0.1, 0, f'delta a = {-np.exp(zc[1]):.3f}*epsilon^{zc[0]:.3f}')
+ax[0].set_xlabel('3-Delta')
+ax[0].set_ylabel('delta a')
+ax[1].set_xlabel('3-Delta')
+ax[1].set_ylabel('delta c')
+
+plt.savefig(f'./data/{theory_name}_3-Delta_delta_ac_plot.png')
+
+plt.show()
