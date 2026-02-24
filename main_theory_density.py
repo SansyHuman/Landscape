760b55d3a2bd4ac65a72ff6ad8fd90ee6c203a4d
SansyHuman <suboo0308@gmail.com>
2026-02-24 15:26:06 +0900

    Fixed error in calculating grid index of charge. Added codes to plot and save local maxima of density.

--- main_theory_density.py
+++ main_theory_density.py
@@ -62,7 +62,6 @@ plt.style.use('default')
 plt.rcParams['figure.figsize'] = (16, 12)
 plt.rcParams['font.size'] = 15
 
-
 def density_unnormalized():
     grid_size = float(input("Enter grid size: "))
 
@@ -79,7 +78,7 @@ def density_unnormalized():
     max_density = 0.0
 
     for ac in ac_data:
-        a_index, c_index = int(ac[0] / grid_size), int(ac[1] / grid_size)
+        a_index, c_index = int((ac[0] - grid_min_a) / grid_size), int((ac[1] - grid_min_c) / grid_size)
         Density[c_index, a_index] += 1.0
         if Density[c_index, a_index] > max_density:
             max_density = Density[c_index, a_index]
@@ -89,13 +88,51 @@ def density_unnormalized():
 
     print(f'Maximum density: {max_density}')
 
-    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-    ax.plot_surface(A, C, Density)
-    ax.contour(A, C, Density, zdir='z', offset=-10, cmap='coolwarm')
+    min_maxima_distance = int(input("Enter minimum grid distance to identify local maxima of density: "))
+    local_maxima_index = peak_local_max(Density, min_distance=min_maxima_distance)
+
+    local_max_a = []
+    local_max_c = []
+    local_max_density = []
+    for i in range(len(local_maxima_index)):
+        if local_maxima_index[i, 0] >= A.shape[0] or local_maxima_index[i, 1] >= A.shape[1]:
+            continue
+        a = A[local_maxima_index[i, 0], local_maxima_index[i, 1]]
+        c = C[local_maxima_index[i, 0], local_maxima_index[i, 1]]
+        density = Density[local_maxima_index[i, 0], local_maxima_index[i, 1]]
+        local_max_a.append(a)
+        local_max_c.append(c)
+        local_max_density.append(density)
+
+    with open(f'./data/landscape_ac_density_grid_{grid_size}_maxima_distance_{min_maxima_distance}.csv', 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(['a', 'c', 'density'])
+        for i in range(len(local_max_a)):
+            csvwriter.writerow([local_max_a[i], local_max_c[i], local_max_density[i]])
+
+    log_density = np.log(Density + 1)
+
+    fig = plt.figure()
+    fig.suptitle('Density in ac space')
+
+    ax = fig.add_subplot(1, 2, 1, projection='3d')
+    ax.set_title(f'Grid size: {grid_size}')
+    ax.plot_surface(A, C, log_density, cmap=cm.coolwarm)
+    ax.contour(A, C, log_density, zdir='z', offset=-10, cmap='coolwarm')
     ax.set_xlabel('a')
     ax.set_ylabel('c')
-    ax.set_zlabel('Density')
-    ax.set_zlim(-10, max_density)
+    ax.set_zlabel('log (Density + 1)')
+    ax.set_zlim(-10, np.log(max_density + 1))
+
+    ax = fig.add_subplot(1, 2, 2)
+    ax.set_title(f'Minimal peak distance: {min_maxima_distance}')
+    pos = ax.imshow(log_density, extent=[na[0], na[-1], nc[0], nc[-1]], cmap=cm.coolwarm, origin='lower')
+    fig.colorbar(pos, ax=ax)
+    ax.plot(local_max_a, local_max_c, 'gx')
+    ax.set_xlabel('a')
+    ax.set_ylabel('c')
+
+    plt.savefig(f'./data/landscape_ac_density_grid_{grid_size}_maxima_distance_{min_maxima_distance}.png')
 
     plt.show()
 
@@ -121,7 +158,7 @@ def density_inverse():
     max_density = 0.0
 
     for ac in ac_data:
-        a_index, c_index = int((1.0 / ac[0]) / grid_size), int((1.0 / ac[1]) / grid_size)
+        a_index, c_index = int((1.0 / ac[0] - grid_min_a) / grid_size), int((1.0 / ac[1] - grid_min_c) / grid_size)
         Density[c_index, a_index] += 1.0
         if Density[c_index, a_index] > max_density:
             max_density = Density[c_index, a_index]
@@ -149,9 +186,17 @@ def density_inverse():
 
     with open(f'./data/landscape_inverse_ac_density_grid_{grid_size}_maxima_distance_{min_maxima_distance}.csv', 'w', newline='') as csvfile:
         csvwriter = csv.writer(csvfile)
-        csvwriter.writerow(['a', 'c', 'density'])
+        csvwriter.writerow(['1/a', '1/c', 'min a', 'max a', 'min c', 'max c', 'density', 'real density'])
         for i in range(len(local_max_a)):
-            csvwriter.writerow([1 / local_max_a[i], 1 / local_max_c[i], local_max_density[i]])
+            real_a_range = (1 / (local_max_a[i] + grid_size), 1 / local_max_a[i])
+            real_c_range = (1 / (local_max_c[i] + grid_size), 1 / local_max_c[i])
+            real_da = real_a_range[1] - real_a_range[0]
+            real_dc = real_c_range[1] - real_c_range[0]
+            real_density = local_max_density[i] * (grid_size * grid_size) / (real_da * real_dc)
+
+            csvwriter.writerow([local_max_a[i], local_max_c[i],
+                                real_a_range[0], real_a_range[1], real_c_range[0], real_c_range[1],
+                                local_max_density[i], real_density])
 
     log_density = np.log(Density + 1)
 
@@ -171,7 +216,7 @@ def density_inverse():
     ax.set_title(f'Minimal peak distance: {min_maxima_distance}')
     pos = ax.imshow(log_density, extent=[na[0], na[-1], nc[0], nc[-1]], cmap=cm.coolwarm, origin='lower')
     fig.colorbar(pos, ax=ax)
-    ax.plot(local_max_a, local_max_c, 'rx')
+    ax.plot(local_max_a, local_max_c, 'gx')
     ax.set_xlabel('1/a')
     ax.set_ylabel('1/c')
 
@@ -189,9 +234,9 @@ def density_log():
     grid_size = float(input("Enter grid size: "))
 
     grid_min_a = float(int(min_log_24a))
-    grid_max_a = float(int(max_log_24a + 24))
+    grid_max_a = float(int(np.log(24 * (max_a * np.e))))
     grid_min_c = float(int(min_log_48c))
-    grid_max_c = float(int(max_log_48c + 48))
+    grid_max_c = float(int(np.log(48 * (max_c * np.e))))
 
     na = np.array(np.arange(grid_min_a, grid_max_a, grid_size))
     nc = np.array(np.arange(grid_min_c, grid_max_c, grid_size))
@@ -201,7 +246,7 @@ def density_log():
     max_density = 0.0
 
     for ac in ac_data:
-        a_index, c_index = int(np.log(24 * ac[0]) / grid_size), int(np.log(48 * ac[1]) / grid_size)
+        a_index, c_index = int((np.log(24 * ac[0]) - grid_min_a) / grid_size), int((np.log(48 * ac[1]) - grid_min_c) / grid_size)
         Density[c_index, a_index] += 1.0
         if Density[c_index, a_index] > max_density:
             max_density = Density[c_index, a_index]
@@ -211,16 +256,76 @@ def density_log():
 
     print(f'Maximum density: {max_density}')
 
-    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+    min_maxima_distance = int(input("Enter minimum grid distance to identify local maxima of density: "))
+    local_maxima_index = peak_local_max(Density, min_distance=min_maxima_distance)
 
-    ax.plot_surface(A, C, np.log(Density + 1), cmap=cm.coolwarm)
-    ax.contour(A, C, np.log(Density + 1), zdir='z', offset=-10, cmap='coolwarm')
+    local_max_a = []
+    local_max_c = []
+    local_max_density = []
+
+    for i in range(len(local_maxima_index)):
+        if local_maxima_index[i, 0] >= A.shape[0] or local_maxima_index[i, 1] >= A.shape[1]:
+            continue
+        a = A[local_maxima_index[i, 0], local_maxima_index[i, 1]]
+        c = C[local_maxima_index[i, 0], local_maxima_index[i, 1]]
+        density = Density[local_maxima_index[i, 0], local_maxima_index[i, 1]]
+        local_max_a.append(a)
+        local_max_c.append(c)
+        local_max_density.append(density)
+
+    with open(f'./data/landscape_log_ac_density_grid_{grid_size}_maxima_distance_{min_maxima_distance}.csv', 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(['log 24a', 'log 48c', 'min a', 'max a', 'min c', 'max c', 'density', 'real density'])
+        for i in range(len(local_max_a)):
+            real_a_range = (np.exp(local_max_a[i]) / 24, np.exp(local_max_a[i] + grid_size) / 24)
+            real_c_range = (np.exp(local_max_c[i]) / 48, np.exp(local_max_c[i] + grid_size) / 48)
+            real_da = real_a_range[1] - real_a_range[0]
+            real_dc = real_c_range[1] - real_c_range[0]
+            real_density = local_max_density[i] * (grid_size * grid_size) / (real_da * real_dc)
+
+            csvwriter.writerow([local_max_a[i], local_max_c[i],
+                                real_a_range[0], real_a_range[1], real_c_range[0], real_c_range[1],
+                                local_max_density[i], real_density])
+
+    log_density = np.log(Density + 1)
+
+    fig = plt.figure()
+    fig.suptitle('Density in log ac space')
+
+    ax = fig.add_subplot(1, 2, 1, projection='3d')
+    ax.set_title(f'Grid size: {grid_size}')
+    ax.plot_surface(A, C, log_density, cmap=cm.coolwarm)
+    ax.contour(A, C, log_density, zdir='z', offset=-10, cmap='coolwarm')
     ax.set_xlabel('log 24a')
     ax.set_ylabel('log 48c')
     ax.set_zlabel('log (Density + 1)')
     ax.set_zlim(-10, np.log(max_density + 1))
 
+    ax = fig.add_subplot(1, 2, 2)
+    ax.set_title(f'Minimal peak distance: {min_maxima_distance}')
+    pos = ax.imshow(log_density, extent=[na[0], na[-1], nc[0], nc[-1]], cmap=cm.coolwarm, origin='lower')
+    fig.colorbar(pos, ax=ax)
+    ax.plot(local_max_a, local_max_c, 'gx')
+    ax.set_xlabel('log 24a')
+    ax.set_ylabel('log 48c')
+
+    plt.savefig(f'./data/landscape_log_ac_density_grid_{grid_size}_maxima_distance_{min_maxima_distance}.png')
+
     plt.show()
 
 
-density_inverse()
+while True:
+    print("Select program to run:")
+    print("1. Unnormalized density")
+    print("2. Inverse density")
+    print("3. Log density")
+    program_num = int(input('>>'))
+
+    if program_num == 1:
+        density_unnormalized()
+    elif program_num == 2:
+        density_inverse()
+    elif program_num == 3:
+        density_log()
+    else:
+        exit()
