e0757f5d47b9cfcabc66d43eddd59862104e6eb2
SansyHuman <suboo0308@gmail.com>
2026-05-10 22:56:09 +0900

    Added program to do polynomial regression between exponents of SCI and central charges.

--- regression/main_sci_exp_regression.py
+++ regression/main_sci_exp_regression.py
@@ -1,5 +1,6 @@
 import polars as pl
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import PolynomialFeatures
 
@@ -74,8 +75,69 @@ test_c = []
 
 for i in range(test_num):
     test_theory.append(theories_dict[sampled_test.df["Name"][i]])
-    train_a.append(float(sampled_test.df["CentralChargeA"][i]))
-    train_c.append(float(sampled_test.df["CentralChargeC"][i]))
+    test_a.append(float(sampled_test.df["CentralChargeA"][i]))
+    test_c.append(float(sampled_test.df["CentralChargeC"][i]))
     sci = SuperConformalIndex(sampled_test.df["SCI"][i])
     exp_data = [sci.dims[j] if j < len(sci.dims) else 0 for j in range(n_exponents)]
-    train_input.append(exp_data)
+    test_input.append(exp_data)
+
+model_a.fit(train_input, train_a)
+model_c.fit(train_input, train_c)
+
+test_a_pred = model_a.predict(test_input)
+test_c_pred = model_c.predict(test_input)
+r2_a = r2_score(test_a, test_a_pred)
+r2_c = r2_score(test_c, test_c_pred)
+
+r2_per_theory = [[0.0, 0.0] for _ in range(n_theory)]
+test_a_per_theory = [[] for _ in range(n_theory)]
+test_c_per_theory = [[] for _ in range(n_theory)]
+test_a_pred_per_theory = [[] for _ in range(n_theory)]
+test_c_pred_per_theory = [[] for _ in range(n_theory)]
+
+for i in range(test_num):
+    theory_index = test_theory[i]
+    test_a_per_theory[theory_index].append(test_a[i])
+    test_c_per_theory[theory_index].append(test_c[i])
+    test_a_pred_per_theory[theory_index].append(test_a_pred[i])
+    test_c_pred_per_theory[theory_index].append(test_c_pred[i])
+
+for i in range(n_theory):
+    r2_per_theory[i][0] = r2_score(test_a_per_theory[i], test_a_pred_per_theory[i])
+    r2_per_theory[i][1] = r2_score(test_c_per_theory[i], test_c_pred_per_theory[i])
+
+with open(f'../data/regression/sci_exp_regression_{n_exponents}_({min_a}_{max_a})({min_c}_{max_c})_{n_samples}_{poly_deg}.csv', 'w', newline='') as csv_file:
+    writer = csv.writer(csv_file)
+    writer.writerow(['Theory', "R2 of a", "R2 of c"])
+    for i in range(n_theory):
+        writer.writerow([theories[i]] + r2_per_theory[i])
+
+plt.style.use('default')
+plt.rcParams['figure.figsize'] = (16, 12)
+plt.rcParams['font.size'] = 15
+
+fig, ax = plt.subplots(nrows=1, ncols=2, squeeze=True)
+
+fig.suptitle(f'Regression of a/c central charge with SCI exponents\nwith {n_exponents} fit upto {poly_deg} degrees terms')
+
+ax[0].set_title(f'a regression R2={r2_a:.3f}')
+cmap = plt.cm.get_cmap('jet', n_theory)
+for i in range(n_theory):
+    ax[0].scatter(test_a_per_theory[i], test_a_pred_per_theory[i], color=cmap(i), label=theories[i])
+a_range = [min_a, max_a]
+ax[0].plot(a_range, a_range, linestyle='--', color='red', label='Exact')
+ax[0].set_xlabel('Real a')
+ax[0].set_ylabel('Predicted a')
+ax[0].legend()
+
+ax[1].set_title(f'c regression R2={r2_c:.3f}')
+for i in range(n_theory):
+    ax[1].scatter(test_c_per_theory[i], test_c_pred_per_theory[i], color=cmap(i), label=theories[i])
+c_range = [min_c, max_c]
+ax[1].plot(c_range, c_range, linestyle='--', color='red', label='Exact')
+ax[1].set_xlabel('Real c')
+ax[1].set_ylabel('Predicted c')
+ax[1].legend()
+
+plt.savefig(f'../data/regression/sci_exp_regression_{n_exponents}_({min_a}_{max_a})({min_c}_{max_c})_{n_samples}_{poly_deg}.png')
+plt.show()
